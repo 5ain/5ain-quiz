d1666a73a1c14b36d0c5558bc6f24769bef5ed0b
5ain <145506559+5ain@users.noreply.github.com>
2023-09-23 01:48:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function index(){
+    window.location.href = 'index.html';
+}
+
+function quiz1(){
+    window.location.href = 'quiz1.html';
+}
+
+function quiz2(){
+    window.location.href = 'quiz2.html';
+}
+
+function quiz3(){
+    window.location.href = 'quiz3.html';
+}
+
+function quiz4(){
+    window.location.href = 'quiz4.html';
+}
+
+function quiz5(){
+    window.location.href = 'quiz5.html';
+}
+
+function final(){
+    window.location.href = 'final.html';
+}
+
+function errou(){
+    window.location.href = 'errou.html';
+}
